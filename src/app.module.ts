d4fbd595a02f9beea2428de3cdc6552b00d2ead1
abhinav <abhinavsinghkkr@gmail.com>
2022-07-15 01:08:32 +0530

    Add files

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { UserProfilesModule } from './user-profiles/user-profiles.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
+    AuthModule,
+    UsersModule,
+    UserProfilesModule,
+  ],
+  controllers: [AppController],
+})
+export class AppModule {}
